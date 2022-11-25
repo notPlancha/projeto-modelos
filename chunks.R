568d0b569c1060c1b5c8f5dd3ccdea94158675d1
notPlancha <andre.plancha@hotmail.com>
2022-11-25 14:04:34 +0000

    now chunks is seperate file

---
+++ chunks.R
@@ -0,0 +1,37 @@
+## ---- chunk-setup ----
+if (!require("pacman")) install.packages("pacman", repos = "https://cran.radicaldevelop.com/")
+pacman::p_load(
+  here,
+  knitr,
+  kableExtra,
+  gtsummary,
+  ggplot2
+)
+showT <- function(x, ...) {
+  kable(x, ...) %>% 
+    kable_styling(full_width = F, 
+                  position = "left",  
+                  font_size = 10, 
+                  latex_options = c("striped"),
+                  latex_table_env= "RTable")
+}
+opts_chunk$set(
+  warning = TRUE, 
+  message = TRUE,
+  tidy.opts = list(width.cutoff = 40), 
+  tidy = TRUE,
+  fig.cap = " ",
+  fig.env = "marginfigure",
+  fig.width = 4,
+  fig.height = 4
+)
+
+## ---- chunk-listings ----
+listings <- read.csv(here("data", "listings.csv"))
+nrow(listings)
+
+## ---- chunk-listingsClasses ----
+data.frame(
+  row.names = colnames(listings),
+  "type" = sapply(listings, class)
+) %>% showT()
